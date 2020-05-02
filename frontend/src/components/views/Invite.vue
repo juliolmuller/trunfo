53452a2ce37eed8bc3ad1e4e27d28998104f938a
Julio Muller <juliodolago@gmail.com>
2020-05-01 23:09:10 -0300

    Created view to register players

--- frontend/src/components/views/Invite.vue
+++ frontend/src/components/views/Invite.vue
@@ -1,15 +1,80 @@
 <template>
   <v-col md="6">
-    <v-card outlined elevation="8" max-width="780px">
-      <v-card-title primary-title>
-        <h1>{{ game }}</h1>
-      </v-card-title>
+    <v-card outlined elevation="8" max-width="960px">
+      <v-row>
+        <v-col cols="12" md="6" class="d-flex align-center justify-center">
+          <div>
+            <v-img :src="qrCode" aspect-ratio="1.5" contain class="mt-4" />
+            <div class="text-center title mt-2">Identificador:</div>
+            <div id="gameId"><code>{{ game }}</code></div>
+          </div>
+        </v-col>
+        <v-col cols="12" md="6">
+          <v-text-field
+            label="Identificador do Jogo"
+            class="centeredInput"
+            rounded filled single-line
+            color="error"
+            v-model.trim="playerName"
+            v-if="inputVisible"
+            @keyup.enter="addPlayer"
+            autofocus
+          />
+          <v-btn block rounded color="error" @click="addPlayer" v-if="inputVisible">
+            <v-icon class="mr-3">mdi-checkbox-marked-circle</v-icon>
+            Adicionar
+          </v-btn>
+          <v-btn block rounded text @click="inputVisible = true" v-else>
+            <v-icon class="mr-3">mdi-plus-circle-outline</v-icon>
+            Adicionar Jogador
+          </v-btn>
+          <v-list rounded dense>
+            <v-list-item-group>
+              <v-list-item inactive class="text-center">
+                <v-list-item-avatar class="mr-0">
+                  <v-img src="@/assets/loading.svg" />
+                </v-list-item-avatar>
+                <v-list-item-content>
+                  <span class="body-2 grey--text">Aguardando jogadores...</span>
+                </v-list-item-content>
+              </v-list-item>
+              <v-list-item class="font-weight-bold" v-for="player in players" :key="player">
+                <v-list-item-content>
+                <div class="d-flex justify-space-between">
+                  <span>{{ player }}</span>
+                  <v-icon small @click.stop="removePlayer(player)">mdi-close-circle</v-icon>
+                </div>
+                </v-list-item-content>
+              </v-list-item>
+            </v-list-item-group>
+          </v-list>
+        </v-col>
+        <v-col cols="8" offset="2" md="4" offset-md="4">
+          <v-btn
+            color="success"
+            class="mx-auto"
+            rounded block large
+            :disabled="players.length < 2"
+            :to="{ name: 'game', params: { game } }"
+          >Pronto</v-btn>
+        </v-col>
+      </v-row>
     </v-card>
   </v-col>
 </template>
 
 <script>
 export default {
+
+  data: () => ({
+    playerName: '',
+    inputVisible: false,
+    players: [
+      'Julio, o bão!', 'Jordana',
+      'Elisa', 'Renato', 'Texa',
+      'Tina', 'Acácio', 'Ramon',
+    ],
+  }),
 
   props: {
     game: {
@@ -26,9 +91,49 @@ export default {
       return api + url
     },
   },
+
+  methods: {
+    addPlayer() {
+      if (this.playerName) {
+        console.log(`New player submitted: ${this.playerName}`)
+        this.players.unshift(this.playerName)
+      }
+      this.inputVisible = false
+      this.playerName = ''
+    },
+    removePlayer(name) {
+      this.players = this.players.filter((player) => player !== name)
+    },
+  },
 }
 </script>
 
-<style>
+<style scoped>
+#gameId {
+  margin-bottom: 0.5em;
+  line-height: 80%;
+  text-align: center;
+  font-size: 3.5em;
+  font-weight: bolder;
+}
 
+.centeredInput >>> .v-text-field__slot label {
+  width: 100%;
+  margin-left: 5%;
+  text-align: center;
+}
+
+.centeredInput >>> .v-text-field__slot input {
+  color: #333;
+  text-align: center;
+}
+
+.centeredInput >>> .v-text-field__slot input::selection {
+  background-color: #f44336;
+  color: white;;
+}
+
+.centeredInput >>> .v-text-field__details {
+  display: none;
+}
 </style>
