e3776442865f05a8785eeb208cacce2d92bca440
Julio Muller <juliodolago@gmail.com>
2020-09-21 23:55:12 -0300

    Configured websocket and server

--- server/src/index.js
+++ server/src/index.js
@@ -1,2 +1,16 @@
+require('dotenv').config()
+const app = require('express')()
+const http = require('http').Server(app)
+const io = require('socket.io')(http)
 
-console.log('Hello, Trunfo!')
+io.on('connection', (socket) => {
+  console.log('> Novo cliente conectado.')
+
+  socket.on('disconnect', () => {
+    console.log('< Cliente desconectado.')
+  })
+})
+
+http.listen(process.env.PORT, () => {
+  console.log(`Server running on http://localhost:${process.env.PORT}`)
+})
