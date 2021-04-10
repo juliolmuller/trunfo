6cd4597edc8a26376c41a3c50a0c6909cc4b2f6a
Julio Muller <juliodolago@gmail.com>
2021-04-10 02:11:55 -0300

    Renamed web project folder

--- web/src/store/index.js
+++ web/src/store/index.js
@@ -1,9 +1,11 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import root from './root'
+import modules from './modules'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-
-  modules: {},
+  modules,
+  ...root,
 })
