f7493a88d25ab53e5877c5e9fefa8d74c09a05b3
Julio Muller <juliodolago@gmail.com>
2021-04-12 22:31:54 -0300

    Simplified socket channel

--- server/src/channels/index.ts
+++ server/src/channels/index.ts
@@ -9,32 +9,13 @@ function configureChannels(app: Application) {
   }
 
   app.on('connection', (connection: any): void => {
-    // On a new real-time connection, add it to the anonymous channel
-    app.channel('anonymous').join(connection)
+    app.channel('everyone').join(connection)
   })
 
   // eslint-disable-next-line @typescript-eslint/no-unused-vars, no-unused-vars
   app.publish((data: any, hook: HookContext) => {
-    // Here you can add event publishers to channels set up in `channels.ts`
-    // To publish only for a specific event use `app.publish(eventname, () => {})`
-
-    console.log('Publishing all events to all authenticated users. See `channels.ts` and https://docs.feathersjs.com/api/channels.html for more information.'); // eslint-disable-line
-
-    // e.g. to publish all service events to all authenticated users use
-    return app.channel('authenticated')
+    return app.channel('everyone')
   })
-
-  // Here you can also add service specific event publishers
-  // e.g. the publish the `users` service `created` event to the `admins` channel
-  // app.service('users').publish('created', () => app.channel('admins'));
-
-  // With the userid and email organization from above you can easily select involved users
-  // app.service('messages').publish(() => {
-  //   return [
-  //     app.channel(`userIds/${data.createdBy}`),
-  //     app.channel(`emails/${data.recipientEmail}`)
-  //   ];
-  // })
 }
 
 export default configureChannels
