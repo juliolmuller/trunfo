cc8812d90f33f83e5a651d74551b770723bdcaf1
Julio Muller <juliodolago@gmail.com>
2021-04-13 01:55:38 -0300

    Added service name to environment variables

--- server/src/services/index.ts
+++ server/src/services/index.ts
@@ -3,7 +3,10 @@ import GameService from './Game.service'
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars, no-unused-vars
 function configureServices(app: Application) {
-  app.use('/games', new GameService())
+  app.use(
+    `/${process.env.GAME_SERVICE_NAME}`,
+    new GameService(),
+  )
 }
 
 export default configureServices
