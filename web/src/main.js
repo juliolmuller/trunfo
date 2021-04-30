9dcedca85ee3c5b3e1f394315057985306bedd06
Julio Muller <juliodolago@gmail.com>
2021-04-29 22:46:28 -0300

    Disabled PWA compatibility

--- web/src/main.js
+++ web/src/main.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import './plugins/composition-api'
-import './plugins/service-workers'
+// import './plugins/service-workers'
 import App from './components/App'
 import vuetify from './plugins/vuetify'
 import router from './router'
