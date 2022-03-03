9f9ffb93adf01d2c4a1233b0cbe0e76086a781a7
Julio Muller <juliodolago@gmail.com>
2022-03-03 00:45:11 -0300

    feat: create home page

---
+++ src/pages/HomePage/HomePage.tsx
@@ -0,0 +1,85 @@
+import AddIcon from '@mui/icons-material/Add'
+import EnterIcon from '@mui/icons-material/Login'
+import Collapse from '@mui/material/Collapse'
+import Paper from '@mui/material/Paper'
+import TextField from '@mui/material/TextField'
+import { ChangeEvent, useEffect, useRef, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+
+import Button from '~/components/Button'
+
+function HomePage() {
+  const GAME_KEY_LENGTH = 6
+  const navigate = useNavigate()
+  const inputRef = useRef<HTMLInputElement>(null)
+  const [inputVisible, setInputVisible] = useState(false)
+  const [inputValue, setInputValue] = useState('')
+
+  function handleChangeInputValue(event: ChangeEvent<HTMLInputElement>) {
+    const newValue = event.target.value
+    const transformedValue = newValue.trim().toUpperCase()
+    transformedValue.length < GAME_KEY_LENGTH && setInputValue(transformedValue)
+  }
+
+  function handleSearchGame() {
+    // TODO: use firebase search method and navigate
+    console.log(`Searching game ${inputRef.current?.value}...`)
+  }
+
+  useEffect(() => {
+    inputVisible && inputRef.current?.focus()
+  }, [inputVisible])
+
+  return (
+    <Paper
+      elevation={3}
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        gap: 2,
+        width: '100%',
+        maxWidth: 600,
+        m: 2,
+        p: 3,
+      }}
+    >
+      <Collapse in={inputVisible}>
+        <TextField
+          ref={inputRef}
+          hiddenLabel
+          size="medium"
+          value={inputValue}
+          variant="filled"
+          onChange={handleChangeInputValue}
+        />
+      </Collapse>
+      <Button
+        color="success"
+        disabled={inputVisible && inputValue.length < GAME_KEY_LENGTH}
+        fullWidth
+        startIcon={<EnterIcon />}
+        variant="contained"
+        onClick={inputVisible
+          ? handleSearchGame
+          : () => setInputVisible(true)}
+        sx={{ color: 'white' }}
+      >
+        {inputVisible
+          ? 'Entrar'
+          : 'Acessar um jogo existente'}
+      </Button>
+      <Button
+        fullWidth
+        startIcon={<AddIcon />}
+        variant="contained"
+        onClick={() => navigate('/new')}
+        sx={{ color: 'white' }}
+      >
+        Criar um novo jogo
+      </Button>
+    </Paper>
+  )
+}
+
+export default HomePage
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,16 +1,16 @@
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 import { useAuth } from '~/hooks'
 import AppLayout from '~/layouts/AppLayout'
+import HomePage from '~/pages/HomePage'
 import LoginPage from '~/pages/LoginPage'
 
 function PublicRouter() {
   return (
     <Routes>
-      <Route path="/" element={<AppLayout />}>
-        <Route index element={<LoginPage />} />
+      <Route element={<AppLayout />}>
+        <Route path="*" element={<LoginPage />} />
       </Route>
-      <Route path="*" element={<Navigate replace to="/" />} />
     </Routes>
   )
 }
@@ -19,7 +19,7 @@ function AuthRoutes() {
   return (
     <Routes>
       <Route path="/" element={<AppLayout />}>
-        <Route index element={<h1>You&rsquo;re signed in</h1>} />
+        <Route index element={<HomePage />} />
       </Route>
     </Routes>
   )
