577108824283b42bb7f29381d91f321f79ddf99e
Julio Muller <juliodolago@gmail.com>
2022-03-08 01:05:26 -0300

    feat: create view of QR Code and game key

--- src/pages/GameMatchPage/PlayersJoiningView/PlayersJoiningView.tsx
+++ src/pages/GameMatchPage/PlayersJoiningView/PlayersJoiningView.tsx
@@ -1,9 +1,72 @@
+import CopyIcon from '@mui/icons-material/ContentCopy'
+import CircularProgress from '@mui/material/CircularProgress'
+import Grid from '@mui/material/Grid'
+import IconButton from '@mui/material/IconButton'
+import Stack from '@mui/material/Stack'
+import Typography from '@mui/material/Typography'
+import { useMemo, useState } from 'react'
+
 import Paper from '~/components/Paper'
+import { useGame } from '~/hooks'
 
 function PlayersJoiningView() {
+  const [copying, setCopying] = useState(false)
+  const { activeGame } = useGame()
+  const qrCode = useMemo(() => {
+    const size = 480
+    const url = window.location.href
+
+    return `https://api.qrserver.com/v1/create-qr-code?size=${size}x${size}&data=${url}`
+  }, [])
+
+  async function handleCopyKeyToClipboard() {
+    if (activeGame) {
+      setCopying(true)
+      await navigator.clipboard.writeText(activeGame.key)
+      // eslint-disable-next-line no-promise-executor-return
+      await new Promise((resolve) => setTimeout(resolve, 1000))
+      setCopying(false)
+    }
+  }
+
   return (
-    <Paper fullWidth maxWidth="sm">
-      <h1>Players Joining View</h1>
+    <Paper fullWidth maxWidth="inherit">
+      <Grid container>
+        <Grid item xs={12} md={5}>
+          <Stack alignItems="center">
+            <img src={qrCode} height="200" alt="código QR" />
+            <Typography variant="subtitle1" sx={{ mt: 4 }}>
+              Chave do Jogo:
+            </Typography>
+            <Typography
+              component="span"
+              variant="h4"
+              sx={{
+                display: 'inline-flex',
+                alignItems: 'center',
+                gap: 1,
+                borderRadius: 2,
+                background: '#888',
+                px: 1,
+                py: 0.5,
+                fontWeight: 700,
+                letterSpacing: 4,
+              }}
+            >
+              <code>{activeGame?.key}</code>
+              <IconButton size="small" onClick={handleCopyKeyToClipboard}>
+                {copying ? (
+                  <CircularProgress color="inherit" size={24} />
+                ) : (
+                  <CopyIcon />
+                )}
+              </IconButton>
+            </Typography>
+          </Stack>
+        </Grid>
+        <Grid item xs={12} md={7}>
+        </Grid>
+      </Grid>
     </Paper>
   )
 }
