8f4f2820e5ee01ccc56e9ccbce47cbd70df671b3
Julio Muller <juliodolago@gmail.com>
2022-03-23 23:12:07 -0300

    feat: create several utilities to generate data

--- src/utils/game-utils.ts
+++ src/utils/game-utils.ts
@@ -1,11 +1,7 @@
+import { genString } from '.'
 
 export function generateKey() {
-  const NUMERIC_BASE = 36
-  const AFTER_DOT = 2
   const ID_SIZE = 6
 
-  return Math.random()
-    .toString(NUMERIC_BASE)
-    .slice(AFTER_DOT, AFTER_DOT + ID_SIZE)
-    .toUpperCase()
+  return genString(ID_SIZE).toUpperCase()
 }
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,2 +1,3 @@
+export * from './data-gen'
 export * from './game-utils'
 export * from './ui-extension'
