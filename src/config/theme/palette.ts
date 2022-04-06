aeb48799d3eacfa778bcc9d43130aa2b8f940917
Julio Muller <juliodolago@gmail.com>
2022-04-06 00:09:13 -0300

    feat: expand use of MUI's theme API

---
+++ src/config/theme/palette.ts
@@ -0,0 +1,24 @@
+import { PaletteOptions } from '@mui/material/styles'
+
+const paletteOptions: PaletteOptions = {
+  primary: {
+    main: '#ff5b68',
+  },
+  secondary: {
+    main: '#628b48',
+  },
+  error: {
+    main: '#ed1d24',
+  },
+  success: {
+    main: '#6ab547',
+  },
+  warning: {
+    main: '#edd83d',
+  },
+  info: {
+    main: '#276fbf',
+  },
+}
+
+export default paletteOptions
