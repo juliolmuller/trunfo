dc5bfe72808fcd02ae65c3dbbf22e0708b083e89
Julio Muller <juliodolago@gmail.com>
2023-03-01 00:00:42 -0300

    fix: fix broken avatar URL (#18)

--- src/helpers/game-utils.ts
+++ src/helpers/game-utils.ts
@@ -10,7 +10,7 @@ export function generateAvatar(name: string) {
   const url = new URL('https://avatars.dicebear.com/api/initials')
   url.searchParams.append('backgroundColors', 'grey')
   url.searchParams.append('fontSize', '40')
-  url.searchParams.append('bold', '1')
+  url.searchParams.append('bold', 'true')
   url.pathname += `/${decodeURI(name)}.svg`
 
   return url.toString()
