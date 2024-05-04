e8ad3914368ee1078cdd0a97c8e8822ebea4e775
juliolmuller <juliolmuller@outlook.com>
2024-05-04 15:43:30 -0300

    feat: create journey to access a game vis its access key

--- src/services/game.service.ts
+++ src/services/game.service.ts
@@ -72,6 +72,28 @@ function connectToGame(gameId: Game['id'], eventHandler: (game: Game) => void) {
   return () => gameRef.off('value')
 }
 
+async function findGameByKey(gameKey: Game['key']) {
+  const gameRef = database.ref('games')
+  const query = gameRef.orderByChild('key').equalTo(gameKey)
+  const snapshot = await query.once('value')
+  const value = snapshot.val()
+  const error = new Error(`Unable to connect to game with key ${gameKey}`)
+
+  if (!value) {
+    throw error
+  }
+
+  const games = Object.entries(value).map(([gameId, rawValue]) => normalizeGame(gameId, rawValue))
+
+  for (const { id, status } of games) {
+    if (status === GameStatus.PLAYERS_JOINING) {
+      return id
+    }
+  }
+
+  throw error
+}
+
 async function createGame(props: Partial<Omit<Game, 'id'>>) {
   const { key, name, ...rest } = props
   const gameKey = key || generateKey()
@@ -114,7 +136,7 @@ async function addOfflinePlayer(gameId: Game['id'], playerName: string) {
   return thenable.key as Player['id']
 }
 
-async function addCurrentUser(gameId: Game['id'], user: User) {
+async function addSignedUser(gameId: Game['id'], user: User) {
   const thenable = await database.ref(`games/${gameId}/players`).push({
     addedAt: new Date().toISOString(),
     avatar: user.avatar,
@@ -198,10 +220,11 @@ async function updateMatchLog(
 
 export const gameService = {
   connectToGame,
+  findGameByKey,
   createGame,
   updateGame,
   addOfflinePlayer,
-  addCurrentUser,
+  addSignedUser,
   removePlayer,
   reorderPlayers,
   createMatch,
